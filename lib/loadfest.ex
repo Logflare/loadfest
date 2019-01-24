c72138a47480162e16e2b1307e937f2db78205ee
chasers <chase@pagemodified.com>
2019-01-24 16:33:24 -0700

    First

---
+++ lib/loadfest.ex
@@ -0,0 +1,18 @@
+defmodule LoadFest do
+  @moduledoc """
+  Documentation for LoadFest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoadFest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/loadfest_test.exs
@@ -0,0 +1,8 @@
+defmodule LoadFestTest do
+  use ExUnit.Case
+  doctest LoadFest
+
+  test "greets the world" do
+    assert LoadFest.hello() == :world
+  end
+end
