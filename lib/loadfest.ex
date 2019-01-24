7f72d9833aa618c225af9d02253fb007ac9979b9
chasers <chase@pagemodified.com>
2019-01-24 16:49:38 -0700

    Setup config for logflare params

--- lib/loadfest.ex
+++ lib/loadfest.ex
@@ -1,18 +1,36 @@
 defmodule LoadFest do
   @moduledoc """
-  Documentation for LoadFest.
+  Load tester for Logflare.
   """
 
   @doc """
-  Hello world.
+  Posts a lot to a Logflare source.
 
   ## Examples
 
-      iex> LoadFest.hello()
-      :world
-
   """
-  def hello do
-    :world
+  def post() do
+    api_key = Application.get_env(:loadfest, :logflare_api_key)
+    source = Application.get_env(:loadfest, :logflare_source)
+    url = "https://logflare.app/api/logs"
+    user_agent = "Loadfest"
+    line = "Derp"
+
+    headers = [
+      {"Content-type", "application/json"},
+      {"X-API-KEY", api_key},
+      {"User-Agent", user_agent}
+    ]
+
+    for line <- 1..100 do
+      body = Jason.encode!(%{
+        log_entry: line,
+        source: source,
+        })
+
+      line = HTTPoison.post!(url, body, headers)
+      IO.puts(line.status_code)
+    end
   end
+
 end
