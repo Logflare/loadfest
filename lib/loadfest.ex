7913cd34a95e00047ec0d011250fa517dceb5ef6
chasers <chase@pagemodified.com>
2019-01-25 10:18:35 -0700

    Add post_dev method

--- lib/loadfest.ex
+++ lib/loadfest.ex
@@ -37,10 +37,28 @@ defmodule LoadFest do
     end
   end
 
-  def sup_test() do
-    Task.Supervisor.start_child(LoadFest.TaskSupervisor, fn ->
-      IO.puts("derp")
-    end)
+  def post_dev(count) do
+    api_key = "Z0mEDl_ZxB-5"
+    source = "5f3c0076-b5c4-4c35-89d3-578282abf469"
+    url = "http://localhost:4000/api/logs"
+    user_agent = "Test script"
+    line = "Derp"
+
+    headers = [
+      {"Content-type", "application/json"},
+      {"X-API-KEY", api_key},
+      {"User-Agent", user_agent}
+    ]
+
+    body = Jason.encode!(%{
+      log_entry: line,
+      source: source,
+      })
+
+    for n <- 1..count do
+      n = HTTPoison.post!(url, body, headers)
+      IO.puts(n.status_code)
+    end
   end
 
 end
