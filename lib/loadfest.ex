05da95a2937d4bf03c9ddc28274397e0249e7e39
chasers <chase@pagemodified.com>
2019-02-05 20:56:44 -0700

    Cleanup

--- lib/loadfest.ex
+++ lib/loadfest.ex
@@ -4,44 +4,34 @@ defmodule LoadFest do
   """
 
   @doc """
-  Posts a lot to a Logflare source.
+  Posts async a lot to a Logflare source.
 
   ## Examples
 
   """
   def post_async(count, env) do
-    key = String.to_atom("logflare_api_key" <> "_" <> env)
-    source_key = String.to_atom("logflare_source" <> "_" <> env)
-    endpoint = String.to_atom("logflare_endpoint" <> "_" <> env)
-
-    api_key = Application.get_env(:loadfest, key)
-    source = Application.get_env(:loadfest, source_key)
-    url = Application.get_env(:loadfest, endpoint)
-    user_agent = "Loadfest"
-
-    headers = [
-      {"Content-type", "application/json"},
-      {"X-API-KEY", api_key},
-      {"User-Agent", user_agent}
-    ]
-
     for line <- 1..count do
       Task.Supervisor.start_child(LoadFest.TaskSupervisor, fn ->
-        body = Jason.encode!(%{
-          log_entry: line,
-          source: source,
-          })
-        ### Should pull metrics from HTTPoison to do this correctly.
-        prev = System.monotonic_time()
-        request = HTTPoison.post!(url, body, headers, hackney: [pool: :loadfest_pool])
-        next = System.monotonic_time()
-        diff = next - prev
-        IO.puts("#{request.status_code} | #{diff/1000000}ms")
+        post(line, env)
       end)
     end
   end
 
+  @doc """
+  Posts synchronously to a Logflare source.
+
+  ## Examples
+
+  """
+
   def post_sync(count, env) do
+    for line <- 1..count do
+      post(line, env)
+      Process.sleep(1000)
+    end
+  end
+
+  defp post(line, env) do
     key = String.to_atom("logflare_api_key" <> "_" <> env)
     source_key = String.to_atom("logflare_source" <> "_" <> env)
     endpoint = String.to_atom("logflare_endpoint" <> "_" <> env)
@@ -57,19 +47,17 @@ defmodule LoadFest do
       {"User-Agent", user_agent}
     ]
 
-    for line <- 1..count do
-      body = Jason.encode!(%{
-        log_entry: line,
-        source: source,
-        })
+    body = Jason.encode!(%{
+      log_entry: line,
+      source: source,
+      })
 
-      prev = System.monotonic_time()
-      request = HTTPoison.post!(url, body, headers)
-      next = System.monotonic_time()
-      diff = next - prev
-      IO.puts("#{request.status_code} | #{diff/1000000}ms")
-      Process.sleep(1000)
-    end
+    ### Should pull metrics from HTTPoison to do this correctly.
+    prev = System.monotonic_time()
+    request = HTTPoison.post!(url, body, headers, hackney: [pool: :loadfest_pool])
+    next = System.monotonic_time()
+    diff = next - prev
+    IO.puts("#{request.status_code} | #{diff/1000000}ms")
   end
 
 end
