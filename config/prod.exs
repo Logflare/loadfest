b6cf957aecc730fb6fa335cdb243d4d8cdba6b36
TzeYiing <ty@tzeyiing.com>
2024-06-26 19:09:20 +0800

    feat: reduce load testing sources

--- config/prod.exs
+++ config/prod.exs
@@ -9,13 +9,13 @@ config :loadfest,
     "loadfest.test.1",
     "loadfest.test.2",
     "loadfest.test.3",
-    "loadfest.test.4",
-    "loadfest.test.5",
-    "loadfest.test.6",
-    "loadfest.test.7",
-    "loadfest.test.8",
-    "loadfest.test.9",
-    "loadfest.test.10"
+    # "loadfest.test.4",
+    # "loadfest.test.5",
+    # "loadfest.test.6",
+    # "loadfest.test.7",
+    # "loadfest.test.8",
+    # "loadfest.test.9",
+    # "loadfest.test.10"
   ]
 
 # max_rps: 40
