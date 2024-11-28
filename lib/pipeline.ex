521c36cdd74bdf4e21dc59c77eda833f82bb1b26
TzeYiing <ty@tzeyiing.com>
2024-11-29 00:15:04 +0800

    feat: increase batch sizes

--- lib/pipeline.ex
+++ lib/pipeline.ex
@@ -17,7 +17,7 @@ defmodule Loadfest.Pipeline do
     end
 
     def handle_demand(demand, state) when demand > 0 do
-      num = Enum.random([1, 50, 250])
+      num = Enum.random([1, 50, 250, 500, 1000])
       text = Enum.random(["aaa","bbb", "ccc", "ddd", "eee"])
 
       messages =
