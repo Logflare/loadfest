62ab8d923d30ae62e872d39682040cc759471994
Brian <brianshan@gmail.com>
2026-08-13 12:42:09 +0100

    Allow setting logflare endpoint from env

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,3 +4,8 @@ if System.get_env("LOGFLARE_PUBLIC_API_KEY") do
   config :loadfest,
     api_key: System.get_env("LOGFLARE_PUBLIC_API_KEY")
 end
+
+if System.get_env("LOADFEST_ENDPOINT") do
+  config :loadfest,
+    endpoint: System.get_env("LOADFEST_ENDPOINT")
+end
